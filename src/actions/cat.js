5dad03076feb5bf2c8d9e6d13329a93d4e60eedf
Selvin Norman <selvin715@gmail.com>
2018-10-12 16:44:08 -0700

    added  fetch async action

---
+++ src/actions/cat.js
@@ -0,0 +1,28 @@
+
+export const FETCH_CAT_SUCCESS = 'FETCH_CAT_SUCCESS';
+export const fetchCatSuccess = catList => ({
+  type: FETCH_CAT_SUCCESS,
+  catList
+})
+
+export const FETCH_CAT_FAILURE = 'FETCH_CAT_FAILURE';
+export const fetchCatFailure = err => ({
+  type: FETCH_CAT_FAILURE,
+  err
+})
+
+export const fetchCat = () => dispatch => {
+  fetch('localhost:8080/api/cat')
+  .then(res => {
+    if (!res.ok) {
+      return Promise.reject(res.statusText);
+    }
+    return res.json();
+  }).then(catList=> {
+    dispatch(fetchCatSuccess(catList));
+  })
+  .catch(err => {
+    dispatch(fetchCatFailure(err));
+  });
+};
+
---
+++ src/actions/dog.js
@@ -0,0 +1,28 @@
+
+export const FETCH_DOG_SUCCESS = 'FETCH_DOG_SUCCESS';
+export const fetchDogSuccess = dogList => ({
+  type: FETCH_DOG_SUCCESS,
+  dogList
+})
+
+export const FETCH_DOG_FAILURE = 'FETCH_DOG_FAILURE';
+export const fetchDogFailure = err => ({
+  type: FETCH_DOG_FAILURE,
+  err
+})
+
+export const fetchDog = () => dispatch => {
+  fetch('localhost:8080/api/dog')
+  .then(res => {
+    if (!res.ok) {
+      return Promise.reject(res.statusText);
+    }
+    return res.json();
+  }).then(dogList=> {
+    dispatch(fetchDogSuccess(dogList));
+  })
+  .dogch(err => {
+    dispatch(fetchDogFailure(err));
+  });
+};
+
