43712014c4085c286ce9e0e3330620e766cf5790
Selvin Norman <selvin715@gmail.com>
2018-10-12 15:00:25 -0700

    retrieved initial dog and cat

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,82 +1,76 @@
 import React from 'react';
 //import {connect} from react-redux;
 export default function Dashboard(props) {
-  console.log('props.catToAdopt: ', props.catToAdopt);
-  function adoptIt() {
-    if(props.catToAdopt.chosen === true) {
-      props.dispatch(props.catToAdopt);
-    } else {
-      props.dispatch(props.dogToAdopt);
-    }
-  }   
+  console.log('props.catToAdopt[0]: ', props.catToAdopt[0]);
+
+  function getCat() {
+    return props.catToAdopt[0];
+  }  
+
+  function getDog() {
+    return props.dogToAdopt;
+  }
+  getCat();
+ 
+  getDog();
+function adoptIt () {
+  console.log(true);
+}
+
+    const catAnimalName = props.catToAdopt[0].name;
+    const catImageDescription = props.catToAdopt[0].imageDescription;     
+    const catSex = props.catToAdopt[0].sex;    
+    const catAge = props.catToAdopt[0].age;    
+    const catBreed = props.catToAdopt[0].breed;    
+    const catStory = props.catToAdopt[0].story;    
+    const catImageURL = props.catToAdopt[0].imageURL; 
     
-  if (props.catToAdopt.chosen === true) {
-    const animalName = props.catToAdopt.name;
-    const imageDescription = props.catToAdopt.imageDescription;     
-    const sex = props.catToAdopt.sex;    
-    const age = props.catToAdopt.age;    
-    const breed = props.catToAdopt.breed;    
-    const story = props.catToAdopt.story;    
-    const imageURL = props.catToAdopt.imageURL; 
+
+    const dogAnimalName = props.dogToAdopt[0].name;    
+    const dogImageDescription = props.dogToAdopt[0].imageDescription; 
+    const dogSex = props.dogToAdopt[0].sex;    
+    const dogAge = props.dogToAdopt[0].age;    
+    const dogBreed = props.dogToAdopt[0].breed;    
+    const dogStory = props.dogToAdopt[0].story;    
+    const dogImageURL = props.dogToAdopt[0].imageURL; 
     return (
       <React.Fragment>
         <section className="catToAdopt">
           <header className="profile">
-            <h2 className="animalName">{animalName}</h2>
-            <img className="animalimageURL" src={imageURL} alt={imageDescription} />
+            <h2 className="animalName">{catAnimalName}</h2>
+            <img className="animalimageURL" src={catImageURL} alt={catImageDescription} />
           </header>
+          <main>
+          <dl className="details">
+            <dt>{catAnimalName}</dt>
+            <dd>{catSex}</dd>
+            <dd>{catAge}</dd>
+            <dd>{catBreed}</dd>
+            <dd>{catStory}</dd>
+          </dl>
+          <button onClick={adoptIt} type="button">Adopt></button>
+        </main>           
         </section>
+
         <section className="dogToAdopt">
           <header className="profile">
-            <h2 className="animalName">{animalName}</h2>
-            <img className="animalimageURL" src={imageURL} alt={imageDescription} />
+            <h2 className="animalName">{dogAnimalName}</h2>
+            <img className="animalimageURL" src={dogImageURL} alt={dogImageDescription} />
           </header>
-        </section>
-        <main>
+          <main>
           <dl className="details">
-            <dt>{animalName}</dt>
-            <dd>{sex}</dd>
-            <dd>{age}</dd>
-            <dd>{breed}</dd>
-            <dd>{story}</dd>
+            <dt>{dogAnimalName}</dt>
+            <dd>{dogSex}</dd>
+            <dd>{dogAge}</dd>
+            <dd>{dogBreed}</dd>
+            <dd>{dogStory}</dd>
           </dl>
           <button onClick={adoptIt} type="button">Adopt></button>
         </main> 
-      </React.Fragment>
-    );
-   } else {
-    const animalName = props.dogToAdopt.name;    
-    const imageDescription = props.dogToAdopt.imageDescription; 
-    const sex = props.dogToAdopt.sex;    
-    const age = props.dogToAdopt.age;    
-    const breed = props.dogToAdopt.breed;    
-    const story = props.dogToAdopt.story;    
-    const imageURL = props.dogToAdopt.imageURL; 
-    return (
-      <React.Fragment>
-        <section className="catToAdopt">
-          <header className="profile">
-            <h2 className="animalName">{animalName}</h2>
-            <img className="animalimageURL" src={imageURL} alt={imageDescription} />
-          </header>
         </section>
-        <section className="dogToAdopt">
-          <header className="profile">
-            <h2 className="animalName">{animalName}</h2>
-            <img className="animalimageURL" src={imageURL} alt={imageDescription} />
-          </header>
-        </section>
-        <main>
-          <dl className="details">
-            <dt>{animalName}</dt>
-            <dd>{sex}</dd>
-            <dd>{age}</dd>
-            <dd>{breed}</dd>
-            <dd>{story}</dd>
-          </dl>
-          <button onClick={adoptIt} type="button">Adopt></button>
-        </main> 
+
       </React.Fragment>
+
     );
   }
-}
+
